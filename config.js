52239b0ce2a29946dbfdd09b323af03582d25c4e
Jhonatan reynel hernandez <Jhonatan.reynel814@pascualbravo.edu.co>
2026-07-09 23:13:39 -0500

    nuevos cambios)

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Reemplaza estos dos valores con los de TU proyecto de Supabase:
 // Project Settings → API → Project URL / anon public key
 // ============================================================
-window.SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-window.SUPABASE_ANON_KEY = "TU-ANON-KEY-AQUI";
+window.SUPABASE_URL = "https://xqtydehareriztdpeooz.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxdHlkZWhhcmVyaXp0ZHBlb296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzAwODIsImV4cCI6MjA5OTIwNjA4Mn0.NhT4o7DtAF53JOt4rwtt21iY8pwqSZ7D2gfyhPL5jaQ";
